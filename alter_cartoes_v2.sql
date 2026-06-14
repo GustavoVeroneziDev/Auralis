ddadf3ca2f8456fbc5f46f5b70fcad119ae423e3
Gustavo Veronezi <gustavoveronezi21@gmail.com>
2026-06-14 00:20:03 -0300

    remoção de arquivos temporários

--- alter_cartoes_v2.sql
+++
@@ -1,5 +0,0 @@
--- Adiciona coluna para rastrear o Registro de preview da fatura aberta
--- Execute no phpMyAdmin antes de testar o preview de fatura
-ALTER TABLE FaturaCartao
-    ADD COLUMN FKRegistroPreview VARCHAR(36) DEFAULT NULL
-    AFTER FKRegistroPagamento;
