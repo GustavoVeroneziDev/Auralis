6a78fb333efa2d92b449f66d5a93cf58d6dca061
Gustavo Veronezi <gustavoveronezi21@gmail.com>
2026-06-14 11:49:28 -0300

    texto

--- limpar_sufixo_parcelas.sql
+++
@@ -1,8 +0,0 @@
--- Remove o sufixo " (X/N)" da Descricao de lançamentos parcelados
--- que já têm ParcelaAtual e TotalParcelas preenchidos.
--- Rodar uma vez no phpMyAdmin.
-
-UPDATE LancamentoCartao
-SET Descricao = TRIM(REGEXP_REPLACE(Descricao, ' \\([0-9]+/[0-9]+\\)$', ''))
-WHERE TotalParcelas IS NOT NULL
-  AND Descricao REGEXP ' \\([0-9]+/[0-9]+\\)$';
