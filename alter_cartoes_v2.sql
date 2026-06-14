c9afa3d8470f422cec802c0c419a63e68c74a418
Gustavo Veronezi <gustavoveronezi21@gmail.com>
2026-06-14 02:07:21 -0300

    "

--- alter_cartoes_v2.sql
+++
@@ -1,6 +0,0 @@
--- Adiciona coluna FKRegistroPreview em FaturaCartao
--- Execute este script no phpMyAdmin antes de usar o sistema de cartões
-
-ALTER TABLE FaturaCartao
-    ADD COLUMN FKRegistroPreview VARCHAR(36) DEFAULT NULL
-    AFTER FKRegistroPagamento;
